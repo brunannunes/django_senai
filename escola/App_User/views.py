b5f0b4275b8f384ce822e24a6242ed21d9548c65
brunannunes <brunanunes324@gmail.com>
2024-05-16 16:14:01 -0300

    funcionou muito

--- escola/App_User/views.py
+++ escola/App_User/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
+
+def formulario_novo_user(request):
+    return render(request, 'cad_user.html')
+
+
+
+@login_required
+def salvar_usuario(request):
+    usuario = request.POST.get('usuario')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    
+    usuario_logado = request.user.username
+
+
+    usuario_logado = request.user.username
+    if(usuario != None and usuario != '' and email != None and email != '' and senha != None and senha != ''):
+        try:
+            tem_usuario = User.objects.get(username=usuario)
+            if(tem_usuario):
+                message.info(request, "Usuario ja existe, tente outro.")
+                return render(request, 'cad_user.html')
+        except User.DoesNoExit:
+            dados_usuario = User.objects.create_user(username=usuario, email=email, password=senha)
+            dados_usuario.save()
+            message.info(request, "Usuario salvo com sucesso")
+            return render(request, 'cad_user.html', {'usuario_logado':usuario_logado})
 
 # Create your views here.
--- escola/escola/urls.py
+++ escola/escola/urls.py
@@ -1,8 +1,11 @@
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('App_Escola.urls')),
+    path('', include('App_Escola.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('usuarios/', include('App_User.urls')),
+
+
 ]
